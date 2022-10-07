cc1554856d32164748f3132c4cf964becaf8f921
Marta Leandro faria romao de figueiredo <maleandr@c2r4s2.42lisboa.com>
2022-10-07 19:36:46 +0100

    ola

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,3 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int gethoessize(const char *buffer, int end) //guarda um numero de caracters tipo buffer size no buffer
+{
+    int i;
+
+    i = -1;
+    while (buffer[++i] && buffer[i] != '\n')
+        ;
+    return (i + (buffer[i] == '\n')); // adiciona mais um pk segunda parte 'e true ent e 1 ni  ee
+}
